a3218b268ff45bd1dc1694e0f228ce07e0965b9c
1nt3ger <arnavtripathy01@gmail.com>
2024-08-17 22:41:44 -0500

    gv
    n

---
+++ assets/scripts/jump_pad.gd
@@ -0,0 +1,15 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(func(body: Node3D) -> void: enter(body))
+
+
+
+
+func enter(body: Node3D) -> void:
+	if not body.is_in_group("Player"):
+		return
+	var player: CharacterBody3D = body
+	player.velocity.y = 50  # Boost the player upward by setting the y velocity
--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export_subgroup("Physics")
-@export var speed: float = 7.0
+@export var speed: float = 4.0
 @export var acceleration: float = 10.0
 @export var friction: float = 18.0
 
@@ -9,7 +9,7 @@ extends CharacterBody3D
 @export var jump_strength: float = 15.0
 
 @onready var _spring_arm: SpringArm3D = $SpringArm3D
-@onready var _model: MeshInstance3D = $MeshInstance3D
+@onready var _model: Node3D = $character_knight
 
 func _ready():
 	pass
@@ -19,7 +19,7 @@ func _physics_process(delta: float):
 	
 	# Get input
 	input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	input_vector = input_vector.rotated(_spring_arm.rotation.y)
+	input_vector = input_vector.rotated(-_spring_arm.rotation.y)
 
 	# Handle x & z: walking
 	var velocity_xz := Vector2(velocity.x, velocity.z)
@@ -39,10 +39,11 @@ func _physics_process(delta: float):
 		_model.rotation.y = look_direction.angle()
 	
 	# Handle y: jumping and gravity
+	print(velocity.y)
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	else:
-		velocity.y = 0
+		# velocity.y = 0
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = jump_strength
 		
