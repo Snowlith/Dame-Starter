07af4e633e2dd0d3eb50c38c0c0b4e4354289735
1nt3ger <arnavtripathy01@gmail.com>
2024-08-20 23:45:19 -0500

    sghdjgk

--- assets/inventory/inventory_ui.gd
+++ assets/inventory/inventory_ui.gd
@@ -1,10 +1,16 @@
 extends Control
 
+@onready var inventory: Inventory = preload("res://assets/inventory/playerinv.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 var is_open = false
 
 func _ready():
+	update_slots()
 	close()
 	
+func update_slots():
+	for i in range(min(inventory.items.size(), slots.size())):
+		slots[i].update(inventory.items[i])
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("inventory"):
 		if is_open:
