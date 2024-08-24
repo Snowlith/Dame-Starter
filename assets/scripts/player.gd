4ef1abfbd8884f597cd6c0c35faec78b222649df
Snowlith <vilmtorm@gmail.com>
2024-08-23 21:25:23 -0500

    Added coyote time

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,7 +1,5 @@
 extends CharacterBody3D
 
-
-
 @export_group("Physics")
 @export var speed: float = 10.0
 @export var acceleration: float = 10.0
@@ -21,6 +19,9 @@ extends CharacterBody3D
 @export var crouch_offset_y: float = 0.5
 @export var crouch_speed: float = 10
 
+@export_subgroup("Quality of Life")
+@export var coyote_time: float = 0.2
+
 @onready var cam: FPSCamera3D = $FPSCamera3D
 
 @onready var stand_col: CollisionShape3D = $StandCollider
@@ -28,6 +29,8 @@ extends CharacterBody3D
 @onready var shape_cast: ShapeCast3D = $CrouchCollider/ShapeCast3D
 
 var current_sprint_time := max_sprint_time
+var current_coyote_time: float = 0
+
 var is_sprinting := false
 var is_crouching := false
 
@@ -43,12 +46,12 @@ func _ready():
 func _physics_process(delta: float):
 	var input_vector := Vector2.ZERO
 	
-	# Get the normalized input vector (movement direction) rotated towards direction faced
 	if not SceneManager.in_transition:
+		# Get the normalized input vector (movement direction) rotated towards direction faced
 		input_vector = Input.get_vector("left", "right", "up", "down").rotated(-rotation.y)
 		
-		is_crouching = _handle_crouch(Input.is_action_pressed("crouch"))
-		is_sprinting = _handle_sprint(Input.is_action_pressed("sprint"), input_vector, delta)
+		_handle_crouch(Input.is_action_pressed("crouch"))
+		_handle_sprint(Input.is_action_pressed("sprint"), input_vector, delta)
 	
 	# Handle x & z: walking
 	var velocity_xz := Vector2(velocity.x, velocity.z)
@@ -64,11 +67,7 @@ func _physics_process(delta: float):
 	velocity.z = velocity_xz.y
 	
 	# Handle y: jumping and gravity
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	else:
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = jump_strength
+	_handle_jump(Input.is_action_just_pressed("jump"), delta)
 		
 	move_and_slide()
 	
@@ -79,34 +78,51 @@ func _get_speed() -> float:
 		return speed * crouch_speed_multiplier
 	return speed
 	
-func _handle_crouch(crouch_input: bool) -> bool:
+func _handle_crouch(crouch_input: bool) -> void:
 	# Keep crouching when banging head on ceiling
 	if is_crouching and not crouch_input:
 		if shape_cast and shape_cast.is_colliding():
-			return true
+			is_crouching = true
+			return
 	
 	# Alternate between colliders
 	stand_col.disabled = crouch_input
 	crouch_col.disabled = not crouch_input
-	return crouch_input
+	is_crouching = crouch_input
 
-func _handle_sprint(sprint_input: bool, input_vector: Vector2, delta: float) -> bool:
+func _handle_sprint(sprint_input: bool, input_vector: Vector2, delta: float) -> void:
 	# When not sprinting, recharge sprint time
+	is_sprinting = false
 	if not sprint_input or is_crouching or is_on_wall():
 		if input_vector == Vector2.ZERO:
 			current_sprint_time += stand_recharge_rate * delta
 		else:
 			current_sprint_time += walk_recharge_rate * delta
 		current_sprint_time = clamp(current_sprint_time, 0, max_sprint_time)
-		return false
+		return
 	
 	# When sprinting, drain sprint time
 	current_sprint_time -= delta
 	if current_sprint_time <= 0:
 		current_sprint_time = 0
-		return false
+		return
 	
-	return true
+	is_sprinting = true
+
+func _handle_jump(jump_input: bool, delta: float) -> void:
+	if is_on_floor():
+		current_coyote_time = coyote_time
+		if jump_input:
+			velocity.y = jump_strength
+			current_coyote_time = 0
+	else:
+		current_coyote_time = max(current_coyote_time - delta, 0)
+		
+		if current_coyote_time > 0 and jump_input:
+			velocity.y = jump_strength
+			current_coyote_time = 0
+		else:
+			velocity.y -= gravity * delta
 
 func _reset_crouch() -> void:
 	_handle_crouch(false)
@@ -123,4 +139,3 @@ func _update_crouch_offset(delta: float) -> Vector3:
 	else:
 		crouch_offset_pos.y = lerp(crouch_offset_pos.y, 0.0, crouch_speed * delta)
 	return crouch_offset_pos
-	
