1dd76e9f87b837cbf28efa303112636585b8e1f7
Snowlith <vilmtorm@gmail.com>
2024-08-25 01:01:19 -0500

    Added item bob

--- systems/inventory/dropped_item.gd
+++ systems/inventory/dropped_item.gd
@@ -5,12 +5,19 @@ class_name DroppedItem
 @export var item: Item
 @onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 
+var bob_time: float
+
+# TODO: make items fall on ready
+
 func _ready() -> void:
+	bob_time += randf_range(0, PI)
 	if not item:
 		despawn()
 	mesh_instance.mesh = item.mesh
 
 func _process(delta) -> void:
+	bob_time += delta
+	mesh_instance.global_position = transform.origin + Vector3(0, sin(bob_time) * 0.1, 0)
 	mesh_instance.rotate_y(delta)
 
 func despawn() -> void:
