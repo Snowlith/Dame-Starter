f0b6e1b933b2b2db5a8362ce60d0a2b071af9a9e
Snowlith <vilmtorm@gmail.com>
2025-01-23 22:58:29 -0600

    Added explosion damage

--- components/range_value/health.gd
+++ components/range_value/health.gd
@@ -17,3 +17,18 @@ func _on_depleted():
 		get_tree().create_timer(effect.lifetime+2).timeout.connect(func(): effect.queue_free())
 		effect.emitting = true
 	get_parent_entity().queue_free()
+
+func decrease(amount_lost: int):
+	super(amount_lost)
+	var label = Label3D.new()
+	get_tree().current_scene.add_child(label)
+	label.billboard = BaseMaterial3D.BILLBOARD_FIXED_Y
+	label.pixel_size = 0.01
+	label.modulate = Color.RED
+	label.text = str(amount_lost)
+	label.global_position = get_parent_entity().global_position
+	var target_position = label.global_position + Vector3.UP * 2
+	var pos_tween = create_tween().bind_node(label)
+	pos_tween.tween_property(label, "global_position", target_position, 2)
+	get_tree().create_timer(2).timeout.connect(func(): label.queue_free())
+	
--- scenes/rocket_bullet.gd
+++ scenes/rocket_bullet.gd
@@ -1,10 +1,9 @@
 extends ItemBehavior
 class_name ProjectileBehavior
 
-@export var damage: int = 100
+@export var damage: Curve
+@export var knockback: Curve
 @export var speed: float = 50
-@export var knockback: float = 100
-@export var explosion_curve: Curve
 
 @onready var rigid_body: RigidBody3D = $"."
 @onready var shape_cast = $ShapeCast3D
@@ -31,24 +30,35 @@ func _physics_process(delta):
 	
 	if not rigid_body.get_colliding_bodies().is_empty():
 		rigid_body.freeze = true
+		rigid_body.rotation = -Vector3.FORWARD
 		shape_cast.force_shapecast_update()
+		#DebugDraw3D.draw_sphere(shape_cast.global_position, explosion_radius, Color.YELLOW, 1)
+		#DebugDraw3D.draw_position(Transform3D(Basis(), shape_cast.global_position), Color.GREEN_YELLOW, 1)
 		for i in shape_cast.get_collision_count():
 			var collider = shape_cast.get_collider(i)
-			var collision_point = shape_cast.get_collision_point(i)
 			var entity = collider as Entity
 			if not entity:
 				continue
-			var explosion_state = entity.get_component(ExplosionState)
-			if not explosion_state:
-				continue
+			var collision_point = shape_cast.get_collision_point(i)
 			var displacement = collision_point - rigid_body.global_position
 			#DebugDraw3D.draw_arrow(rigid_body.global_position, collision_point, Color.GREEN, 0.1, true, 10)
 			var distance = displacement.length()
 			var direction = displacement.normalized()
 			#DebugDraw3D.draw_arrow(collision_point, direction, Color.GREEN, 0.1, true, 10)
-			var explosion_amount = explosion_curve.sample(distance / explosion_radius)
-			explosion_state.receive_impulse(direction * explosion_amount * knockback)
-			DebugDraw3D.draw_arrow(entity.global_position, entity.global_position + explosion_amount * direction * knockback / 50, Color.RED, 0.1, true, 10)
+			var normalized_falloff = distance / explosion_radius
+			var health: Health = entity.get_component(Health)
+			if health:
+				var damage_amount = damage.sample(normalized_falloff)
+				health.decrease(damage_amount)
+			var explosion_state = entity.get_component(ExplosionState)
+			if explosion_state:
+				var explosion_amount = knockback.sample(normalized_falloff)
+				explosion_state.receive_impulse(direction * explosion_amount)
+				DebugDraw3D.draw_arrow(entity.global_position, entity.global_position + explosion_amount * direction / 50, Color.RED, 0.1, true, 10)
+			elif entity.get_physics_body() is RigidBody3D:
+				var rigid_body: RigidBody3D = entity.get_physics_body()
+				var explosion_amount = knockback.sample(normalized_falloff)
+				rigid_body.apply_central_impulse(direction * explosion_amount)
 		queue_free()
 	else:
 		DebugDraw3D.draw_arrow(_previous_position, _current_position, Color.BLUE, 0.1, true, 2)
